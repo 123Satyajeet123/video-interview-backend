9601f66cbfd03291597387f8859d8eafec9b927e
pranjalanimesh <pranjalanimesh5@gmail.com>
2025-05-14 18:24:34 +0530

    basic boilerplate

---
+++ index.js
@@ -0,0 +1,26 @@
+// Importing the env variables
+require("dotenv").config();
+
+// Importing the required modules
+const express = require("express");
+
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+});
+
+// Starting the server
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
+
